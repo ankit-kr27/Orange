8fa352c500f73624771cd1701812fbe87675b365
xnki7 <codeinja7@gmail.com>
2024-09-18 12:15:30 +0530

    merge fix

--- Frontend/src/components/register-component/register-component.tsx
+++ Frontend/src/components/register-component/register-component.tsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
-import React from 'react'
+import React from "react"
 
->>>>>>> 103c51511c21a2d39748916f1192c018b1f43fbf
 const RegisterComponent = () => {
   return (
     <div>
